0ca332abc7d393eebcfbaabceeb91bf4c473fcd4
nop <noplab90@gmail.com>
2025-09-28 19:16:57 +0900

    Current

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,11 @@ use panic_halt as _;
 #[cfg(feature = "defmt")]
 use {defmt_rtt as _, panic_probe as _};
 
+use fmt::*;
+
 use embassy_executor::Spawner;
 use embassy_stm32::{
+    adc::{Adc, AdcChannel},
     gpio::{Level, Output, Speed},
     opamp::{OpAmp, OpAmpSpeed},
     time::Hertz,
@@ -30,19 +33,19 @@ pub async fn led_task(
     mut led3: Output<'static>,
 ) {
     loop {
-        led1.set_low();
-        led2.set_high();
-        led3.set_high();
-        Timer::after(Duration::from_millis(500)).await;
-
         led1.set_high();
         led2.set_low();
-        led3.set_high();
+        led3.set_low();
         Timer::after(Duration::from_millis(500)).await;
 
-        led1.set_high();
+        led1.set_low();
         led2.set_high();
         led3.set_low();
+        Timer::after(Duration::from_millis(500)).await;
+
+        led1.set_low();
+        led2.set_low();
+        led3.set_high();
         Timer::after(Duration::from_millis(500)).await;
     }
 }
@@ -52,7 +55,33 @@ pub async fn motor_task() {}
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
-    let p = embassy_stm32::init(Default::default());
+    let mut config = embassy_stm32::Config::default();
+    {
+        use embassy_stm32::rcc::Pll;
+        use embassy_stm32::rcc::PllMul;
+        use embassy_stm32::rcc::PllPreDiv;
+        use embassy_stm32::rcc::PllRDiv;
+        use embassy_stm32::rcc::PllSource;
+
+        config.rcc.hsi = true;
+        config.rcc.pll = Some(Pll {
+            source: PllSource::HSI,
+            prediv: PllPreDiv::DIV4,
+            mul: PllMul::MUL85,
+            divp: None,
+            divq: None,
+            divr: Some(PllRDiv::DIV2),
+        });
+
+        use embassy_stm32::rcc::mux::Adcsel;
+        use embassy_stm32::rcc::mux::ClockMux;
+
+        let mut clock_mux = ClockMux::default();
+        clock_mux.adc12sel = Adcsel::SYS;
+        config.rcc.mux = clock_mux;
+    }
+
+    let p = embassy_stm32::init(config);
 
     let led1 = Output::new(p.PC13, Level::High, Speed::Low);
     let led2 = Output::new(p.PC14, Level::High, Speed::Low);
@@ -60,12 +89,20 @@ async fn main(spawner: Spawner) {
 
     spawner.spawn(led_task(led1, led2, led3)).unwrap();
 
+    let mut adc1 = Adc::new(p.ADC1);
+    let mut adc2 = Adc::new(p.ADC2);
+
     let mut op1 = OpAmp::new(p.OPAMP1, OpAmpSpeed::Normal);
-    op1.standalone_int(p.PA1, p.PA3);
+    let op1_sa = op1.standalone_int(p.PA1, p.PA3);
+    let mut op1_adc_ch = op1_sa.degrade_adc();
+
     let mut op2 = OpAmp::new(p.OPAMP2, OpAmpSpeed::Normal);
-    op2.standalone_int(p.PA7, p.PC5);
+    let op2_sa = op2.standalone_int(p.PA7, p.PC5);
+    let mut op2_adc_ch = op2_sa.degrade_adc();
+
     let mut op3 = OpAmp::new(p.OPAMP3, OpAmpSpeed::Normal);
-    op3.standalone_int(p.PB0, p.PB2);
+    let op3_sa = op3.standalone_int(p.PB0, p.PB2);
+    let mut op3_adc_ch = op3_sa.degrade_adc();
 
     let mut uvw_pwm = ComplementaryPwm::new(
         p.TIM1,
@@ -104,6 +141,10 @@ async fn main(spawner: Spawner) {
     uvw_pwm.disable(Channel::Ch3);
     uvw_pwm.set_dead_time(1);
 
+    uvw_pwm.enable(Channel::Ch1);
+    uvw_pwm.enable(Channel::Ch2);
+    uvw_pwm.enable(Channel::Ch3);
+
     let mut angle = 0.0f32;
     let max_duty = 99u16;
     let mut angular_velocity = 0.01f32; // 初期角速度（ラジアン/ループ）
@@ -137,6 +178,12 @@ async fn main(spawner: Spawner) {
             // 2π
             angle -= 6.283185;
         }
+
+        let op1_val = adc1.blocking_read(&mut op1_adc_ch);
+        let op2_val = adc2.blocking_read(&mut op2_adc_ch);
+        let op3_val = adc1.blocking_read(&mut op3_adc_ch);
+
+        info!("{}, {}, {}", op1_val, op2_val, op3_val);
 
         Timer::after(Duration::from_millis(1)).await;
     }
